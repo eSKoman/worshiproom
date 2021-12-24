fbc80f4ee4fbec8336b2a21f0d088681f3145e90
eSKoman <kotlar.slavomir@gmail.com>
2021-12-24 01:56:45 +0100

    Galéria a slideshow

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.php"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'josefin': ['"Josefin Sans"']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
